33572f1dda422a0208911529efe2e19f49ebb9e1
Juhyunsoo <amature0000@naver.com>
2024-11-16 02:12:12 +0900

    cuda minibech update

--- cuda_bench.py
+++ cuda_bench.py
@@ -3,10 +3,17 @@ import time
 
 def gpu_benchmark():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    print(torch.__version__)
-    print(torch.version.cuda)
-    print(torch.cuda.is_available())
+    print(f"{torch.__version__=}")
+    print(f"{torch.version.cuda=}")
+    print(f"{torch.cuda.is_available()=}")
     print(f"사용 중인 장치: {device}")
+    if torch.cuda.is_available():
+        print(f"GPU 개수: {torch.cuda.device_count()}")
+        for i in range(torch.cuda.device_count()):
+            print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+        print(f"CUDA 버전: {torch.version.cuda}")
+    else:
+        print("CUDA를 사용할 수 없습니다.")
 
     # 큰 텐서를 GPU에 생성
     x = torch.randn((10000, 10000), device=device)
