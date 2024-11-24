d5faa5470f4427b682a74305da914e8b0fdbd070
Juhyunsoo <amature0000@naver.com>
2024-11-24 16:13:26 +0900

    2nd task

--- grid_surviver(WSJ)/evaluate.py
+++ grid_surviver(WSJ)/evaluate.py
@@ -10,53 +10,41 @@ class GridSurvivorRLAgent(GridSurvivorAgent):
         self.agent = QLearningAgent(3, 2 ** 12)
         self.state = State()
         self.device = DEVICE
-        self.ev = False
 
-    def act(self, obs):
-        self.state.process_state(obs)
-        # print(self.state.features(), end = '  ')
-        # self.agent.print_q_value(self.state.features())
-        return self.agent.choose_action(self.state.features(), self.ev)
+    def act(self, obs=None):
+        if obs != None: self.state.process_state(obs)
+        return self.agent.choose_action(self.state.features(), True)
     
     def load(self):
         self.agent.load_model("q_save")
-        self.ev = True
     
-    def train(self):
-        current_episode = 0
-        env = make_grid_survivor(show_screen=SC)
-        next_state = State()
+    def iter_eval(self):
+        total_bee = 0
         total_step = 0
-        ema = 0.0
         best = 0
+        episode = 0
         try:
             while True:
+                env = make_grid_survivor(show_screen=SC)
                 current_step = 0
                 obs, _ = env.reset()
-
+                self.state.process_state(obs)
+                episode += 1
                 while True:
                     current_step += 1
-                    action = self.act(obs)
+                    action = self.act()
                     
-                    next_obs, _, terminated, truncated, _ = env.step(action)
-                    next_state.process_state(next_obs)
-                    reward = process_reward(self.state, next_state)
-                    done = terminated or truncated
-                    # if _reward == -10.0: done = True
-                    # self.agent.store_transition(self.state.features(), action, reward, next_state.features(), done)
-
-                    self.agent.learn(self.state.features(), action, reward, next_state.features())
-                    if done: break
-                    obs = next_obs
-                current_episode += 1
+                    obs, _, terminated, truncated, _ = env.step(action)
+                    self.state.process_state(obs)
+                    if terminated or truncated: break
                 total_step += current_step
-                best = max(best, 50-next_state.b)
-                ema = (ema * 0.9) + ((50-next_state.b) * 0.1)
-                print("bee : ", 50 - next_state.b, " / actions : ", current_step, " / epsilon : ", self.agent.epsilon, "lr : ", self.agent.lr, " / total step : ", total_step, " / ema : ", ema, " / best : ", best)
-                self.agent.save_model("q_save")
+                bee = 50 - self.state.b
+                total_bee += bee
+                best = max(best, bee)
+                print(",", bee)
+                #print("bee : ", bee, " / actions : ", current_step, " / total step : ", total_step, " / total bee : ", total_bee, " / eval step : ", total_step / episode, " / eval bee : ", total_bee / episode, " / best : ", best)
         except KeyboardInterrupt:
             print("Ctrl-C -> Exit")
-        finally:
             env.render()
             env.close()
 
@@ -64,5 +52,5 @@ class GridSurvivorRLAgent(GridSurvivorAgent):
 if __name__ == "__main__":
     agent = GridSurvivorRLAgent()
     agent.load()
-    agent.train()
+    agent.iter_eval()
     #evaluate(agent)
